bcf59cc84e04b2c24ee1ab96e5928acf505d2814
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-09-28 09:17:33 +0100

    added basic client styling

---
+++ client/index.js
@@ -0,0 +1,8 @@
+function searchResult(){
+    console.log('testing')
+    document.getElementById("centerPage").style.top = "10%";
+    document.getElementById("search_bar").style.display = "none";
+    document.getElementById("search_bar").style.display = "none";
+    document.getElementById("submitButton1").style.display = "none";
+    document.getElementById("submitButton2").style.display = "none";
+}
