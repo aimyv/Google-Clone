2523ed73bd72dc1fbb097166fc53cc2498058a83
aimyv <av533@bath.ac.uk>
2022-09-28 11:19:32 +0100

    redefined data structure

--- controllers/searches.js
+++ controllers/searches.js
@@ -1,24 +1,24 @@
 const express = require('express');
-const Search = require('../models/search')
+const SearchItem = require('../models/search')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const search = Search.all
+    const search = SearchItem.all
     res.send(search)
 })
 
-router.get('/:item', (req, res) => {
-    try {
-        const searchItem = req.params.item;
-        const selectedSearch = Search.findById(searchItem);
-        if (!selectedSearch) {
-            throw new Error('This search does not exist!')
-        }
-        res.send(selectedSearch);
-    } catch (err) {
-        console.log(err);
-        res.status(404).send({message: err.message})
-    }
-})
+// router.get('/:item', (req, res) => {
+//     try {
+//         const searchItem = req.params.item;
+//         const selectedSearch = Search.findById(searchItem);
+//         if (!selectedSearch) {
+//             throw new Error('This search does not exist!')
+//         }
+//         res.send(selectedSearch);
+//     } catch (err) {
+//         console.log(err);
+//         res.status(404).send({message: err.message})
+//     }
+// })
 
 module.exports = router;
