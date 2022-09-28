427b28a15cc906796fb0204f367d0e8520963097
aimyv <av533@bath.ac.uk>
2022-09-28 10:31:19 +0100

    added server2 folders

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const server = express()
+
+let cors = require("cors")
+server.use(cors());
+
+// ################################ Google ################################
+server.get('/', (req, res) => {
+    res.send('Testing Lap1 Group exercise')
+})
+
+server.get('/google', (req, res) => {
+    res.send(google)
+})
+
+server.get('/google/:id', (req, res) => {
+    const id = parseInt(req.params.id) - 1;
+    res.send(turtle[id])
+})
+
+module.exports = server
+// // ######################################## Turtle ########################################
+// app.get('/turtle', (req, res) => {
+//     res.send(turtle)
+// })
+
+// app.get('/turtle/:id', (req, res) => {
+//     const id = parseInt(req.params.id) - 1;
+//     res.send(turtle[id])
+// })
+
+// // ######################################## Tiger ########################################
+// app.get('/tiger', (req, res) => {
+//     res.send(tiger)
+// })
+
+// app.get('/tiger/:id', (req, res) => {
+//     const id = parseInt(req.params.id) - 1;
+//     res.send(tiger[id])
+// })
+
+// // ######################################## Koala ########################################
+// app.get('/koala', (req, res) => {
+//     res.send(koala)
+// })
+
+// app.get('/koala/:id', (req, res) => {
+//     const id = parseInt(req.params.id) - 1;
+//     res.send(koala[id])
+// })
+
+
+
