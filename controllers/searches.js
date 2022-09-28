ebc3245c59f8caf9e46d3265b4d2c30bf4186a9d
aimyv <av533@bath.ac.uk>
2022-09-28 16:07:30 +0100

    added random function for other queries

--- controllers/searches.js
+++ controllers/searches.js
@@ -18,6 +18,39 @@ router.get('/random', (req, res) => {
     }
 })
 
+router.get('/random/turtle', (req, res) => {
+    try {
+        const num = Math.floor(Math.random() * 10) + 1;
+        const selectedSearch = SearchItem.findById(num);
+        res.send(selectedSearch);
+    } catch(err) {
+        console.log(err);
+        res.status(404).send({message: err.message});
+    }
+})
+
+router.get('/random/tiger', (req, res) => {
+    try {
+        const num = Math.floor(Math.random() * (20 - 11 + 1) ) + 11;
+        const selectedSearch = SearchItem.findById(num);
+        res.send(selectedSearch);
+    } catch(err) {
+        console.log(err);
+        res.status(404).send({message: err.message});
+    }
+})
+
+router.get('/random/koala', (req, res) => {
+    try {
+        const num = Math.floor(Math.random() * (30 - 21 + 1) ) + 21;
+        const selectedSearch = SearchItem.findById(num);
+        res.send(selectedSearch);
+    } catch(err) {
+        console.log(err);
+        res.status(404).send({message: err.message});
+    }
+})
+
 router.get('/:id', (req, res) => {
     try {
         const searchItem = parseInt(req.params.id);
