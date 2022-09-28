fa4a8c5a1304784d511dc394c1ecfb19e1a826bd
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-09-28 16:17:42 +0100

    added search results styling and reformatted some html

--- client/index.js
+++ client/index.js
@@ -1,9 +1,10 @@
 function searchResult(){
     console.log('testing')
     document.getElementById("centerPage").style.top = "10%";
-    document.getElementById("search_bar").style.display = "none";
-    document.getElementById("submitButton1").style.display = "none";
-    document.getElementById("submitButton2").style.display = "none";
+    document.getElementById("centerPage").style.marginTop = "5%";
+    // document.getElementById("search_bar").style.display = "none";
+    // document.getElementById("submitButton1").style.display = "none";
+    // document.getElementById("submitButton2").style.display = "none";
 }
 let index=1;
 const resultList = document.getElementById('divResult');
@@ -34,25 +35,27 @@ async function append(input){
         let div = document.createElement('div')
         div.className = `div${i}`
         resultList.appendChild(div)
-        
+        let gap = document.createElement('br')
+
         let li = document.createElement('li');
         li.textContent = googleData2.breadcrumb
         li.className = "breadCrumb"
         div.appendChild(li); 
+        div.appendChild(gap)
 
         let li2 = document.createElement('a');
         li2.textContent = googleData2.title;
         li2.className = "title"
         li2.href = googleData2.url
         div.appendChild(li2); 
+        div.appendChild(gap)
 
         let li3 = document.createElement('li');
         li3.textContent = googleData2.description
         li3.className = "description"
         div.appendChild(li3); 
-
-        let gap = document.createElement('br')
         div.appendChild(gap)
+        
     }
 }
 
